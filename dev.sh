ad7fb20e82d5c431e83056835fe2a492dbe792cb
Thomas Bottini <thomas.bottini@cnrs.fr>
2022-10-07 14:35:05 +0200

    •

--- dev.sh
+++ dev.sh
@@ -7,4 +7,5 @@ cd ./data
 curl "https://opentheso.huma-num.fr/opentheso/api/all/theso?id=th358&format=jsonld" -o thesaurus.json 
 cd ../..
 
+npx gatsby clean
 npm start
--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,32 +1,26 @@
-import graphql from "gatsby"
-import * as React from "react"
+import graphql from 'gatsby'
+import * as React from 'react'
 import Layout from '../components/layout'
 
 const BlogPost = (props) => {
   console.log(props)
-  return "a"
-  // return <Layout pageTitle="mon titre 🦖">coucou</Layout>
+  return <Layout pageTitle="mon titre 🦖">coucou</Layout>
 }
 
-// export const pageQuery = graphql`
-//   query BlogPostBySlug($slug: String!) {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       id
-//       excerpt(pruneLength: 160)
-//       html
-//       frontmatter {
-//         title
-//       }
-//       fields {
-//         date(formatString: "MMMM DD, YYYY")
-//       }
-//     }
-//   }
-// `
+export const q = graphql`
+  query MyQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug }}) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+      }
+      fields {
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`
 
 export default BlogPost
