664466a2512478b5bf6b79a40b5aa60eaef86471
Félix <someonefefe@gmail.com>
2022-10-07 14:34:39 +0200

    update dev scripts

--- dev.bat
+++ dev.bat
@@ -1,5 +1,7 @@
-cd src/data
-git pull https://github.com/Amleth/ceres-www-content.git
+cd ./src
+if not exist data\ (git clone https://github.com/Amleth/ceres-www-content.git ./data)
+cd ./data
+git pull
 curl "https://opentheso.huma-num.fr/opentheso/api/all/theso?id=th265&format=jsonld" -o thesaurus.json 
 cd ../..
 
--- dev.sh
+++ dev.sh
@@ -1,5 +1,9 @@
-cd src/data
-git pull https://github.com/Amleth/ceres-www-content.git
+cd src/
+if ![ -d "./data" ];
+then
+    git clone https://github.com/Amleth/ceres-www-content.git ./data
+fi
+cd ./data
 curl "https://opentheso.huma-num.fr/opentheso/api/all/theso?id=th358&format=jsonld" -o thesaurus.json 
 cd ../..
 
