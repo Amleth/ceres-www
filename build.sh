ca62728dd80891f6fec761d327c99063a9d8b217
Thomas Bottini <thomas.bottini@cnrs.fr>
2022-10-07 11:07:12 +0200

    th

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd src/data
 git pull https://github.com/Amleth/ceres-www-content.git
-curl "https://opentheso.huma-num.fr/opentheso/api/all/theso?id=th265&format=jsonld" -o thesaurus.json 
+curl "https://opentheso.huma-num.fr/opentheso/api/all/theso?id=th358&format=jsonld" -o thesaurus.json 
 cd ../..
 
 npm run build
